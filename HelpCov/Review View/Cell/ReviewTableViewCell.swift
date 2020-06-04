505446b52d40859b8aa17f8f32fbc334d66990dd
Antoine Simon <antoine.simon@ynap.com>
2020-06-04 10:10:58 +0100

    create skeleton

---
+++ HelpCov/Review View/Cell/ReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewTableViewCell.swift
+//  HelpCov
+//
+//  Created by Antoine Simon on 04/06/2020.
+//  Copyright © 2020 Antoine Simon. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HelpCov/Review View/ReviewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewViewModel.swift
+//  HelpCov
+//
+//  Created by Antoine Simon on 04/06/2020.
+//  Copyright © 2020 Antoine Simon. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewModel: NSObject {
+
+}
